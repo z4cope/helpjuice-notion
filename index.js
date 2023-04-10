59f71d76927c747cb8619b56a81e63b28d6e9883
z4cope <63551733+z4cope@users.noreply.github.com>
2023-04-10 14:31:32 +0200

    Manipulate dom by adding h1 on select menu

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const textParent = document.querySelector("#blog-text-parent");
 const textGeneratorForm = document.querySelector("#text-generator");
 const textInput = document.querySelector("#text-input");
 const selectionWindow = document.querySelector("#selection-window");
+const searchedKeyword = document.querySelector("#keyword");
+const headingOne = document.querySelector("#heading-one");
+const selectItem = document.querySelectorAll(".select-item");
+const selectedItemParent = document.querySelector("#selected-item-parent");
+
+selectItem.forEach((select) => {
+  select.addEventListener("click", () => {
+    switch (select.attributes.id.nodeValue) {
+      case "heading-one":
+        selectedItemParent.innerHTML = `<h1 id="head-1" spellcheck="true" contenteditable="true"></h1>`;
+        break;
+    }
+  });
+});
+
+//Helper functions
+const revealItem = (element) => {
+  element.style.opacity = 1;
+  element.style.zIndex = 1;
+};
 
 //Event Listeners
 textGeneratorForm.addEventListener("submit", (e) => {
@@ -10,14 +30,19 @@ textGeneratorForm.addEventListener("submit", (e) => {
 });
 
 textInput.addEventListener("input", (e) => {
-  console.log(e.target.value);
+  searchedKeyword.innerText = e.target.value;
   switch (e.target.value) {
+    case "/":
+      revealItem(selectionWindow);
+      headingOne.style.display = "none";
+      break;
     case "/1":
-      selectionWindow.style.opacity = 1;
-      selectionWindow.style.zIndex = 1;
+      revealItem(selectionWindow);
+      headingOne.style.display = "flex";
       break;
     default:
       selectionWindow.style.opacity = 0;
       selectionWindow.style.zIndex = 0;
+      headingOne.style.display = "none";
   }
 });
