b427b8df3e642f8f9a1b0aa0c60c9a6b47ba333e
z4cope <63551733+z4cope@users.noreply.github.com>
2023-04-10 13:32:25 +0200

    Add popup menu logic reveal on type

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 //Selectors
 const textParent = document.querySelector("#blog-text-parent");
+const textGeneratorForm = document.querySelector("#text-generator");
+const textInput = document.querySelector("#text-input");
+const selectionWindow = document.querySelector("#selection-window");
+
+//Event Listeners
+textGeneratorForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+textInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  switch (e.target.value) {
+    case "/1":
+      selectionWindow.style.opacity = 1;
+      selectionWindow.style.zIndex = 1;
+      break;
+    default:
+      selectionWindow.style.opacity = 0;
+      selectionWindow.style.zIndex = 0;
+  }
+});
